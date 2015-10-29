c7ba8f8f84e986f93ef962dd063ac5b8304c3f3e
Jeffrey V Wong <jvwong@outlook.com>
2015-10-29 12:15:40 -0400

    tweaked watch tasks

--- tasks/config/watch.js
+++ tasks/config/watch.js
@@ -18,16 +18,20 @@ module.exports = function(grunt) {
 		api: {
 
 			// API files to watch:
-			files: ['api/**/*', '!**/node_modules/**']
-		},
-		assets: {
-
-			// Assets to watch:
-			files: ['assets/**/*', 'tasks/pipeline.js', '!**/node_modules/**'],
+			files: ['api/**/*', '!**/node_modules/**'],
 
 			// When assets are changed:
 			tasks: ['syncAssets' , 'linkAssets']
 		}
+
+		// , assets: {
+		//
+		// 	// Assets to watch:
+		// 	files: ['assets/**/*', 'tasks/pipeline.js', '!**/node_modules/**'],
+		//
+		// 	// When assets are changed:
+		// 	tasks: ['syncAssets' , 'linkAssets']
+		// }
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
